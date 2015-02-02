b6c7db11f62c701adf57dc1667e56249fc11ea24
qqibrow <qqibrow@gmail.com>
2015-02-01 23:59:23 -0800

    finish easy share functionality.

--- app/src/main/java/com/lniu/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/lniu/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,22 +1,39 @@
 package com.lniu.gridimagesearch.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.widget.ShareActionProvider;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.lniu.gridimagesearch.R;
 import com.lniu.gridimagesearch.models.ImageResult;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ImageDisplayActivity extends ActionBarActivity {
 
     ImageView ivFullImage;
     ProgressBar progressBar;
+    ShareActionProvider mShareActionProvider;
+    ImageResult imageResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,28 +43,54 @@ public class ImageDisplayActivity extends ActionBarActivity {
         ivFullImage = (ImageView) findViewById(R.id.ivFullImage);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        ImageResult imageResult = (ImageResult) getIntent().getSerializableExtra("result");
+        imageResult = (ImageResult) getIntent().getSerializableExtra("result");
+
+        // TODO(lniu) Performance loss here.
+        Picasso.with(this).load(imageResult.getFullUrl()).into(target);
         Picasso.with(this).load(imageResult.getFullUrl()).error(R.drawable.ic_action_error)
                 .into(ivFullImage, new Callback.EmptyCallback() {
 
-            @Override
-            public void onSuccess() {
-                progressBar.setVisibility(View.GONE);
-            }
+                    @Override
+                    public void onSuccess() {
+                        progressBar.setVisibility(View.GONE);
+                    }
 
-            @Override
-            public void onError() {
-                progressBar.setVisibility(View.GONE);
-            }
-        });
-
+                    @Override
+                    public void onError() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
+
+    private Target target = new Target() {
+        @Override
+        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+            setupShareIntent(bitmap);
+            Toast.makeText(ImageDisplayActivity.this, "loaded bitmap", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable errorDrawable) {
+
+        }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+        }
+    };
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+       // mShareActionProvider.setShareIntent(getDefaultIntent());
         return true;
     }
 
@@ -59,12 +102,44 @@ public class ImageDisplayActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_share) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void setupShareIntent(Bitmap bmp) {
+        // Fetch Bitmap Uri locally
+        Uri bmpUri = getLocalBitmapUri(bmp); // see previous remote images section
+        if(bmpUri == null)
+            Toast.makeText(this, "bmp uri is empty", Toast.LENGTH_SHORT).show();
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        mShareActionProvider.setShareIntent(shareIntent);
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(Bitmap bmp) {
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
 }
